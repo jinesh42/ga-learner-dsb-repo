6ac45d3337a7215f5c1c73e8654314dffea72fb2
jinesh42 <jinesh42@users.noreply.github.com>
2020-04-19 23:36:39 +0530

    Code file created.

---
+++ my_first_prj/code.py
@@ -0,0 +1,74 @@
+# --------------
+# Code starts here
+
+# Create the lists 
+class_1 = ['Geoffrey Hinton','Andrew Ng','Sebastian Raschka','Yoshua Bengio' ]
+class_2 = ['Hilary Mason','Carla Gentry','Corinna Cortes']
+
+# Concatenate both the strings
+new_class = (class_1 + class_2)
+print (new_class)
+
+
+# Append the list
+new_class.append('Peter Warden')
+
+# Print updated list
+print(new_class)
+
+
+# Remove the element from the list
+new_class.remove('Carla Gentry')
+
+# Print the list
+print(new_class)
+
+# Code ends here
+
+
+# --------------
+# Code starts here
+courses={'Math':65,'English':70,'History':80,'French':70,'Science':60}
+tg=[]
+for a,i in courses.items():
+    tg.append(i)
+
+total=sum(tg)   
+print(total)
+percentage=(total/500)*100
+print(percentage)
+
+# Code ends here
+
+
+# --------------
+# Code starts here
+mathematics={'Geoffrey Hinton':78,'Andrew Ng':95,'Sebastian Raschka':65,
+'Corinna Cortes':66,'Yoshua Benjio':50,'Hilary Mason':75}
+
+topper=max(mathematics,key=mathematics.get)
+print(topper)
+
+
+
+
+
+
+# Code ends here  
+
+
+# --------------
+# Given string
+topper = 'andrew ng'
+
+
+# Code starts here
+
+first_name,last_name=topper.split(" ")
+full_name=last_name + ' ' + first_name
+certificate_name=full_name.upper()
+print(certificate_name)
+
+# Code ends here
+
+
